5c01d8e091da429640de1cac371f44cc21f5a150
PhillMcGough <phillmcgough@gmail.com>
2015-08-24 18:19:13 +0100

    New GitProject

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	$siteTitle = "Img.uk";
+	$siteDesc = "Free image hosting service!";
+?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo "$siteTitle - $siteDesc"; ?></title>
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+		<?php
+		echo "<h1>Welcome to $siteTitle</h1>";
+		?>
+    </body>
+</html>
